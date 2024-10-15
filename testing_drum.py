ca5835247800c140282239241bf09035a2a26320
Tyler Vuong <tylervuong2004@gmail.com>
2024-10-15 13:07:55 -0400

    added comments

--- testing_drum.py
+++ testing_drum.py
@@ -3,15 +3,15 @@ import brickpi3
 import time
 from utils.brick import TouchSensor, EV3UltrasonicSensor, wait_read_sensors, reset_brick
 
-BP = brickpi3.BrickPi3()
+BP = brickpi3.BrickPi3() #initialize brickpi
 MOTOR = BP.PORT_A
-sensor = EV3UltrasonicSensor(3)
+sensor = EV3UltrasonicSensor(3) 
 POWER_LIMIT = 200
 SPEED_LIMIT = 720
 try:
     print("drumming mechanism test")
     try:
-        BP = brickpi3.BrickPi3()
+        BP = brickpi3.BrickPi3() #set everything with limits and initialize sensors
         BP.offset_motor_encoder(MOTOR, BP.get_motor_encoder(MOTOR))
         BP.set_motor_limits(MOTOR,POWER_LIMIT, SPEED_LIMIT)
         BP.set_motor_power(MOTOR,0)
@@ -21,12 +21,12 @@ try:
         print(error)
 
     try: 
-        BP.set_motor_limits(MOTOR, POWER_LIMIT,110)
+        BP.set_motor_limits(MOTOR, POWER_LIMIT,110) 
         time.sleep(5)
-        print(sensor.get_value())
-        BP.set_motor_position_relative(MOTOR,90)
-        time.sleep(5)
-        BP.set_motor_position_relative(MOTOR,-90)
+        print(sensor.get_value()) #get value
+        BP.set_motor_position_relative(MOTOR,90) #rotate motor down
+        time.sleep(5) #wait in between
+        BP.set_motor_position_relative(MOTOR,-90) #rotate motor up
         time.sleep(4)
         print(sensor.get_value())
     
