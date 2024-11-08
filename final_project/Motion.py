2a071c6129354fb15adafceb65909785db0ecc6a
BaptisteDidier <baptiste.didier@yahoo.com>
2024-11-08 16:30:32 -0500

    updates motion

--- final_project/Motion.py
+++ final_project/Motion.py
@@ -2,34 +2,84 @@ import brickpi3
 import time
 
 class Motion:
-    def __init__(self, left_motor_port=brickpi3.BrickPi3.PORT_A, right_motor_port=brickpi3.BrickPi3.PORT_B):
+    def __init__(self):
+        """
+        Initialize a Motion object with motors and factors
+        Resets the motor encoders and stops motors
+        """
         self.BP = brickpi3.BrickPi3()
-        self.left_motor_port = left_motor_port
-        self.right_motor_port = right_motor_port
+        self.left_motor = self.BP.PORT_A
+        self.right_motor = self.BP.PORT_B
+        self.left_factor = self.right_factor = 1
+        self.reset_encoders()
+        self.stop()
+
+
+# Public methods
+    def initialize(self):
+        """
+        Calibrate the motors to adjust for any speed discrepancies
+        """
+        self.BP.set_motor_power(self.left_motor, 50)
+        self.BP.set_motor_power(self.right_motor, 50)
+        time.sleep(1)
+        
+        left = self.BP.get_motor_encoder(self.left_motor)
+        right = self.BP.get_motor_encoder(self.right_motor)
+        factor = min(left, right) / max(left, right)
+        self.left_factor, self.right_factor = (factor, 1) if left > right else (1, factor)
 
     def move_forward(self, speed, duration):
-        self.BP.set_motor_power(self.left_motor_port, speed)
-        self.BP.set_motor_power(self.right_motor_port, speed)
-        time.sleep(duration)
-        self.stop()
+        """
+        Moves forward for a certain duration
+        """
+        self.move(speed, speed, duration)
+
+    def move_backward(self, speed, duration):
+        """
+        Moves backward for a certain duration
+        """
+        self.move(-speed, -speed, duration)
 
     def turn_left(self, speed, duration):
-        self.BP.set_motor_power(self.left_motor_port, -speed)
-        self.BP.set_motor_power(self.right_motor_port, speed)
-        time.sleep(duration)
-        self.stop()
+        """
+        Turns left for a certain duration
+        """
+        self.move(-speed, speed, duration)
 
     def turn_right(self, speed, duration):
-        self.BP.set_motor_power(self.left_motor_port, speed)
-        self.BP.set_motor_power(self.right_motor_port, -speed)
-        time.sleep(duration)
-        self.stop()
+        """
+        Turns right for a certain duration
+        """
+        self.move(speed, -speed, duration)
 
     def stop(self):
-        self.BP.set_motor_power(self.left_motor_port, 0)
-        self.BP.set_motor_power(self.right_motor_port, 0)
+        """
+        Stops any movement
+        """
+        self.move(0, 0, 0)
 
     def reset(self):
+        """
+        Resets the BrickPi
+        """
+        self.stop()
         self.BP.reset_all()
 
 
+# Private methods
+    def _reset_encoders(self):
+        """
+        Resets encoders to 0
+        """
+        self.BP.reset_motor_encoder(self.left_motor)
+        self.BP.reset_motor_encoder(self.right_motor)
+        
+    def _move(self, left_speed, right_speed, duration):
+        """
+        Controls both motors for a certain duration
+        """
+        self.BP.set_motor_power(self.left_motor, left_speed * self.left_factor)
+        self.BP.set_motor_power(self.right_motor, right_speed * self.right_factor)
+        time.sleep(duration)
+        self.stop()
--- final_project/main.py
+++ final_project/main.py
@@ -1,29 +1,25 @@
-import brickpi3
 from Motion import Motion
 from utils.brick import wait_ready_sensors
 
-BP = brickpi3.BrickPi3()
-MOTOR1 = BP.PORT_A
-MOTOR2 = BP.PORT_B
+motion = Motion()
 
 wait_ready_sensors(True)
 print("Done initializing")
 
 def main():
-    motion = Motion(left_motor_port=MOTOR1, right_motor_port=MOTOR2)
     
     try:
+        motion.initialize()
         motion.move_forward(50, 2)
-        motion.turn_left(30, 1)
+        motion.turn_right(50, 2)
+        motion.turn_left(50, 2)
         motion.move_forward(50, 2)
-        motion.turn_right(30, 1)
         
     except KeyboardInterrupt:
         pass
     
     finally:
         print("Stopping and resetting")
-        motion.stop()
         motion.reset()
 
 if __name__ == "__main__":
