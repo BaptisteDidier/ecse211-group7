a5d4bcc30a37c841fa9fe44c4f531f36946bfd1d
BaptisteDidier <baptiste.didier@yahoo.com>
2024-11-13 15:11:48 -0500

    added grabbing

---
+++ final_project/Grabbing.py
@@ -0,0 +1,21 @@
+from utils.brick import Motor
+
+class Grabbing:
+    def __init__(self):
+        self.motor = Motor("A")
+        self.motor_power = 100
+        self.motor_dps = 720
+        self.motor.set_limits(self.motor_power, self.motor_dps)
+        self.motor.reset_encoder()
+        
+    def open(self):
+        """
+        Open the gate
+        """
+        self.motor.set_position_relative(90)
+        
+    def close(self):
+        """
+        Close the gate
+        """
+        self.motor.set_position_relative(-90)
