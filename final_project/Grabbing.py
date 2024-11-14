d0cd94e39aea9d7e0c3c8ed57a6fabceb167da79
BaptisteDidier <baptiste.didier@yahoo.com>
2024-11-13 23:22:03 -0500

    updated colorDetection

--- final_project/Grabbing.py
+++ final_project/Grabbing.py
@@ -1,21 +1,21 @@
 from Ressources import gate_motor
 
-class Grabbing:
-    def __init__(self):
-        self.motor = gate_motor
-        self.motor_power = 100
-        self.motor_dps = 720
-        self.motor.set_limits(self.motor_power, self.motor_dps)
-        self.motor.reset_encoder()
+# Global variables
+motor_power = 100
+motor_dps = 720
+gate_motor.set_limits(motor_power, motor_dps)
+gate_motor.reset_encoder()
+     
+     
+# Public methods   
+def open():
+    """
+    Open the gate
+    """
+    gate_motor.set_position_relative(90)
         
-    def open(self):
-        """
-        Open the gate
-        """
-        self.motor.set_position_relative(90)
-        
-    def close(self):
-        """
-        Close the gate
-        """
-        self.motor.set_position_relative(-90)
+def close():
+    """
+    Close the gate
+    """
+    gate_motor.set_position_relative(-90)
