21a23a306cbcb66203420113debeefd5a292d7c3
BaptisteDidier <baptiste.didier@yahoo.com>
2024-11-14 13:21:13 -0500

    Update Grabbing.py

--- final_project/Grabbing.py
+++ final_project/Grabbing.py
@@ -24,30 +24,33 @@ def get_normalized_rgb():
     """
     Returns the normalized values of the fixed color sensor
     """
-    array = block_color_sensor.get_rgb()
+    array = list()
+    for i in range(5):
+        rgb = block_color_sensor.get_rgb()
+        if any(value is None or value == 0 for value in rgb):
+            print("Invalid reading")
+            return [0, 0, 0]
+        array.append(rgb)
     
-    if any(value is None or value == 0 for value in array):
-        print("Invalid reading")
-        return [0, 0, 0]
+    total = [sum(x) for x in zip(*array)]
+    return [round(255 * c / sum(total), 0) for c in total]
     
-    total = sum(array)
-    return [round(c / total) for c in array]
-    
-def is_valid_block(array):
+def is_valid_block():
     """
     Compares an array of normalized rgb values to see whether it is valid
     """
+    array = get_normalized_rgb()
     if any(value is None or value == 0 for value in array):
         print("Invalid reading")
         return False
     
-    if (array[0] > 35 or array[0] < 55): # need to test to find the correct intervals
+    if (array[0] < 35 or array[0] > 55): # need to test to find the correct intervals
         return False
     
-    if (array[1] > 35 or array[1] < 55): # need to test to find the correct intervals
+    if (array[1] < 35 or array[1] > 55): # need to test to find the correct intervals
         return False
     
-    if (array[2] > 35 or array[2] < 55): # need to test to find the correct intervals
+    if (array[2] < 35 or array[2] > 55): # need to test to find the correct intervals
         return False
     
     return True
