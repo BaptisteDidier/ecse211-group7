c816281c479ca7f6ec838a68236cdc582942885b
BaptisteDidier <baptiste.didier@yahoo.com>
2024-11-14 23:34:55 -0500

    implemented a heuristic + changed costmap

--- final_project/CostMap.py
+++ final_project/CostMap.py
@@ -1,59 +1,40 @@
 from Resources import ultrasonic_sensor
 import Motion
+import math
 
-class CostMap:
-    
-    def __init__(self):
-        """
-        Create a CostMap object to represent the map
-        """
-        self.map_width = 122
-        self.map_height = 122
-        self.cube_size = 2.54
-        self.width = self.map_width // self.cube_size
-        self.height = self.map_height // self.cube_size
-        self.costmap = [[0 for _ in range(self.width)] for _ in range(self.height)]
-        
-    def __str__(self):
-        """
-        Display the cost map
-        """
-        return "\n".join(" ".join(f"{cell:3d}" for cell in row) for row in self.costmap)
+# Global variable
+obstacles = list()
 
-    def add_obstacle(self, obstacle_position):
-        """
-        Add an obstacle to the costmap
-        """
-        x, y = obstacle_position
-        cell_x = int(x // self.cube_size)
-        cell_y = int(y // self.cube_size)
-        if 0 <= cell_x < self.width and 0 <= cell_y < self.height:
-            self.costmap[cell_y][cell_x] = 1
+def get_obstacle_position():
+    """
+    Get the position of the obstacle in front depending on the current position
+    """
+    distance = ultrasonic_sensor.get_cm()
+    if distance is None:
+        return None;
     
-    def add_water(self, water_position):
-        """
-        Add a water portion to the map
-        """
-        x, y = water_position
-        cell_x = int(x // self.cube_size)
-        cell_y = int(y // self.cube_size)
-        if 0 <= cell_x < self.width and 0 <= cell_y < self.height:
-            self.costmap[cell_y][cell_x] = float("inf")
+    x, y, theta = Motion.get_position()
+    new_x = x + math.cos(theta)
+    new_y = y + math.sin(theta)
     
-    def get_cost(self, point):
-        """
-        Get the cost of a point in the costmap
-        """
-        x, y = point
-        cell_x = int(x // self.cube_size)
-        cell_y = int(y // self.cube_size)
-        if 0 <= cell_x < self.width and 0 <= cell_y < self.height:
-            return self.costmap[cell_y][cell_x]
-        return float("inf")
+    if new_x >= 122 or new_y >= 122: # if the sensor sees the walls
+        return None
+    
+    return new_x, new_y
 
-    def scan(self):
-        """
-        Turns to assess where the obstacles are
-        """
-        Motion.turn(50, 180, "right")
+def add_obstacle(obstacle_position):
+    """
+    Add an obstacle to the costmap
+    """
+    if obstacle_position is None or any(value < 0 or value >= 122 for value in obstacle_position):
+        print("Invalid obstacle")
+        return;
+ 
+    obstacles.append(obstacle_position)
+    
+def scan():
+    """
+    Turns to assess where the obstacles are
+    """
+    Motion.turn(50, 180, "right")
         
