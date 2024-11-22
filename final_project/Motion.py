ddfea4fb63e2418b502d057c8b35db5a354a7fc2
BaptisteDidier <baptiste.didier@yahoo.com>
2024-11-22 15:17:59 -0500

    Update Motion.py

--- final_project/Motion.py
+++ final_project/Motion.py
@@ -101,13 +101,17 @@ def turn(speed=40, angle=90, direction='right'):
         if direction == 'right':
             left_motor.set_power(-modular_speed)
             right_motor.set_power(modular_speed)
+            if angle > current_angle:
+                stop()
+                break
         else:
             left_motor.set_power(modular_speed)
             right_motor.set_power(-modular_speed)
+            if angle < current_angle:
+                stop()
+                break
         
-        if abs(angle - current_angle) == 0:
-            stop()
-            break
+    
  
         time.sleep(0.05)
 
