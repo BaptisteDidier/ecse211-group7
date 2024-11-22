85d20afa2683e7a9751ee73dbcd29464d1e4e3ac
Tyler Vuong <tylervuong2004@gmail.com>
2024-11-22 13:37:08 -0500

    added code for robot to stop and turn at wall

--- final_project/WallCheck.py
+++ final_project/WallCheck.py
@@ -1,6 +1,6 @@
 from Resources import *
 import Motion
-
+from Multithread import run_in_background
 
 
 def robot_constant_move():
@@ -10,13 +10,20 @@ def robot_constant_move():
 def check_distance():
     value = ultrasonic_sensor.get_cm()
     print(value)
+    if value > 8 and value < 20:
+        Motion.stop()
+        Motion.turn(30,90, "left")
+        robot_constant_move()
 
 
 def main():
     initialize_components()
     left_motor.set_limits(30,360)
     right_motor.set_limits(30,360)
-    check_distance()
+    robot_constant_move()
+    while True:
+        check_distance()
+    
     
 
 
