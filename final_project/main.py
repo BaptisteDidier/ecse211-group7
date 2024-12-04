011e942a0fc5e104bba6dfe923fc05bc98b56711
BaptisteDidier <baptiste.didier@yahoo.com>
2024-12-04 17:53:23 -0500

    Update main.py

--- final_project/main.py
+++ final_project/main.py
@@ -1,29 +1,17 @@
 from Resources import *
-import threading
-import time
 import Motion
+import Grabbing
 
 initialize_components()
 
 # Main loop
 def main():
     try:
-        #Motion.orient_and_pickup()
-        Motion.thread_turn(30, 90, 'right')
-        #Motion.change_row()
-        #Motion.move(40, 1, 'backward')
-        #print(sweep)
-        #print("3")
-        #Motion.sweep(-45, 45)
-        #print("2")
-        #Motion.thread_sweep()
-        #Motion.move()
-        #print(block_color_sensor.get_rgb())
-        
-        #threads = threading.enumerate()
-        #for thread in threads:
-        #    print(thread.name)
-        #print(ultrasonic_sensor.get_cm())
+        Motion.move(40, 8, 'forward')
+        Motion.orient_and_pickup()
+        Motion.move(40, 15, 'forward')
+        Grabbing.open_gate()
+        Motion.move(40, 15, 'backward')
         
     except KeyboardInterrupt:
         pass
